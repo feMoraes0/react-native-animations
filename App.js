b63d558fe26cf43913f37f4fcebad8d884142beb
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-08-02 21:54:19 -0300

    feat(home): add home screen

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/Home';
 
 export default function App() {
+  const {Navigator, Screen} = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator initialRouteName={Home.name} screenOptions={{ headerMode: 'none'}}>
+        <Screen name={Home.name} component={Home.component} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
